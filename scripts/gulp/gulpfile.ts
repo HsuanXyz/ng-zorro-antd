f9a1b05fde266d2141dc32f8927fa9b4f6a9596e
Hsuan Lee <HsuanGM@gmail.com>
2019-06-21 10:21:05 +0800

    Chore/now preview (#1)
    * chore: sync ant-design v3.19.6 (#3602)
    
    * fix(module:table): fix sortChange with dynamic columns (#3603) (#3605)
    
    close #3603
    
    * chore: sync with antd 3.19 (#3590)
    
    * chore: sync with antd 3.19
    
    * test(module:page-header): fix test
    
    * chore: refactor commit hook (#3610)
    
    * chore: refactor commit hook
    
    * chore(packaging): clean dependencie
    
    * chore(packaging): clean dev dependencie
    
    * chore: refactor commit hook (#3610)
    
    * chore: refactor commit hook
    
    * chore(packaging): clean dependencie
    
    * chore(packaging): clean dev dependencie
    
    * chore: refactor commit hook (#3610)
    
    * chore: refactor commit hook
    
    * chore(packaging): clean dependencie
    
    * chore(packaging): clean dev dependencie
    
    * build: refactor scripts
    
    ref #2474
    
    * chore: update ci config
    
    * ci: fix integration
    
    * ci: add travis_wait
    
    * ci: fix ci
    
    * ci: fix ci
    
    * ci: fix ci
    
    * ci: fix ci
    
    * ci: fix ci
    
    * chore: fix type
    
    * build: add site release scripts
    
    * build: _
    
    * build: add `test:watch` task
    
    * build: fix `test:watch` task
    
    * chore: bump node to 12.1.0
    
    * ci: add now.json

---
+++ scripts/gulp/gulpfile.ts
@@ -0,0 +1,26 @@
+// tslint:disable:no-import-side-effect
+import { series, task } from 'gulp';
+import './tasks/clean';
+import './tasks/default';
+import './tasks/schematic';
+import './tasks/unit-test';
+import './tasks/universal';
+
+import './tasks/library';
+import './tasks/site';
+
+task('build:release', series(
+  'clean',
+  'build:library',
+  'build:release-site'
+));
+
+task('build:preview', series(
+  'clean',
+  'build:simple-site'
+));
+
+task('start:dev', series(
+  'clean',
+  'start:site'
+));
